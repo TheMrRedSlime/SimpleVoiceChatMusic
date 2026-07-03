8de7c360f4fa7a7bf25485c484b9be0493913126
TheMrRedSlime <69750444+TheMrRedSlime@users.noreply.github.com>
2026-07-03 07:12:47 +0530

    help command fix

--- src/main/java/dev/klash/simpleVoiceChatMusic/SimpleVoiceChatMusic.java
+++ src/main/java/dev/klash/simpleVoiceChatMusic/SimpleVoiceChatMusic.java
@@ -7,12 +7,21 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.StringUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Logger;
 
 public final class SimpleVoiceChatMusic extends JavaPlugin {
 
     public static Logger LOGGER;
+    private final List<String> subCommands = Arrays.asList(
+        "play", "search", "playing", "queue", "skip", 
+        "pause", "resume", "stop", "volume", "kill", "bassboost"
+    );
 
     public static SimpleVoiceChatMusic get() {
         return getPlugin(SimpleVoiceChatMusic.class);
@@ -79,7 +88,19 @@ public final class SimpleVoiceChatMusic extends JavaPlugin {
         }
         if(label.equalsIgnoreCase("music") || label.equalsIgnoreCase("svcmusic")) {
             if(args.length == 0) {
-                sender.sendMessage("/music <play/stop/pause/bassboost/kill/playing/queue/resume/search/skip/volume>");
+                sender.sendMessage("""
+                    /music play <song title> - Searches and queues the first result
+                    /music search <song> - Lists all results from YouTube (default) and lets you choose which you want to queue
+                    /music now-playing - Displays the currently playing song
+                    /music queue - Shows the queue
+                    /music skip - Skips the current song
+                    /music pause - Pauses the current song
+                    /music resume - Resumes the current song
+                    /music stop - Stops the current song and clears the queue
+                    /music volume <1-100> - Sets the volume
+                    /music kill - use when something goes wrong and you want to restart the plugin without restarting the server
+                    /music bassboost <0-200> - adds bass boost
+                """);
                 return true;
             }else {
                 try {
@@ -120,5 +141,21 @@ public final class SimpleVoiceChatMusic extends JavaPlugin {
             }
         }
         return super.onCommand(sender, command, label, args);
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (!alias.equalsIgnoreCase("music") && !alias.equalsIgnoreCase("svcmusic")) {
+            return Collections.emptyList();
+        }
+
+        List<String> completions = new ArrayList<>();
+
+        if (args.length == 1) {
+            StringUtil.copyPartialMatches(args[0], subCommands, completions);
+            Collections.sort(completions);
+            return completions;
+        }
+        return Collections.emptyList();
     }
 }
